2ad4908aae535aae897233c6657598b3417baa02
= <=>
2026-02-11 13:31:16 +0530

    add service-descovery

---
+++ api-service-descovery/src/main/java/in/ramesh/discovery/server/ApiServiceDescoveryApplication.java
@@ -0,0 +1,13 @@
+package in.ramesh.discovery.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceDescoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceDescoveryApplication.class, args);
+	}
+
+}
---
+++ api-service-descovery/src/test/java/in/ramesh/discovery/server/ApiServiceDescoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ramesh.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceDescoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
